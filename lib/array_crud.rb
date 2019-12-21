9843fbfa42070874aee6cdd411b6145fc2065a82
Jason Chung <jqc5610@gmail.com>
2019-12-21 17:23:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  add_element_to_start_of_array.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  add_element_to_start_of_array.last
+  array.last
 end
 
 def update_element_from_index(array, index_number, element)
-  add_element_to_start_of_array[1] = "Taco"
+  array[4] = "totally"
 end
